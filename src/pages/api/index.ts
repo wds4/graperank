2c5826e68451f27cb0f3493d462479e08ae6d467
David Strayhorn <david.strayhorn4@gmail.com>
2024-11-10 16:06:09 -0500

    minor edit

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -8,5 +8,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  res.status(200).json({ message: `api Hello from Next.js!! region: ${process.env.AWS_REGION}` })
+  res.status(200).json({ message: `api Hello from Next.js!!! region: ${process.env.AWS_REGION}` })
 }
