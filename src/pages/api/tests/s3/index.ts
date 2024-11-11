a63b7b4c33301d6f437b9e97ed6833075425c6e1
David Strayhorn <david.strayhorn4@gmail.com>
2024-11-11 11:43:27 -0500

    s3 retrieval test

--- src/pages/api/tests/s3/index.ts
+++ src/pages/api/tests/s3/index.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { S3Client, PutObjectCommand, ListBucketsCommand, ListObjectsCommand } from '@aws-sdk/client-s3'
+import { S3Client, GetObjectCommand, PutObjectCommand, ListBucketsCommand, ListObjectsCommand } from '@aws-sdk/client-s3'
 
 const client = new S3Client({
   region: process.env.AWS_REGION,
@@ -14,26 +14,20 @@ const params1 = {
 const params2 = {
   Bucket: 'grapevine-nostr-cache-bucket'
 };
-const params3 = {
-  Bucket: 'grapevine-nostr-cache-bucket',
-  Key: 'whatGoesHere',
-  Body: '{ foo: "bar" }'
-}
-/*
-const params4 = {
-  Bucket: 'grapevine-nostr-cache-bucket',
-  Key: 'whatGoesHere',
-}
-*/
+
 const command1 = new ListBucketsCommand(params1);
 const command2 = new ListObjectsCommand(params2);
-const command3 = new PutObjectCommand(params3);
-// const command4 = new GetObjectCommand(params4);
 
 type ResponseData = {
   success: boolean,
   message: string,
   data?: object,
+}
+
+const fooFxn = async () => {
+  const oOutput = { foo: 'bar', a: 'b' }
+  const sOutput = JSON.stringify(oOutput)
+  return sOutput
 }
  
 export default async function handler(
@@ -43,21 +37,42 @@ export default async function handler(
   try {
     console.log(`key: ${process.env.AWS_ACCESS_KEY_ID}`)
     const data1 = await client.send(command1);
-    console.log(`data1: ${JSON.stringify(data1)}`)
+    console.log(`= data1: ${JSON.stringify(data1)}`)
     
     const data2 = await client.send(command2);
-    console.log(`data2: ${JSON.stringify(data2)}`)
+    console.log(`= data2: ${JSON.stringify(data2)}`)
 
+    /* PutObjectCommand */
+    const params3 = {
+      Bucket: 'grapevine-nostr-cache-bucket',
+      Key: 'whatGoesHere',
+      Body: await fooFxn()
+    }
+    const command3 = new PutObjectCommand(params3);
     const data3 = await client.send(command3);
-    console.log(`data3: ${JSON.stringify(data3)}`)
+    console.log(`= data3: ${JSON.stringify(data3)}`)
 
-    // const data4 = await client.send(command4);
-    // console.log(`=============== data4: ${JSON.stringify(data4)}`)
+    /* GetObjectCommand */
+    
+    const params4 = {
+      Bucket: 'grapevine-nostr-cache-bucket',
+      Key: 'whatGoesHere',
+    }
+    const command4 = new GetObjectCommand(params4);
+    const data4 = await client.send(command4);
+    const foo = await data4.Body?.transformToString()
+
+    let data4_ = ''
+    if (typeof foo == 'string') {
+      data4_ = JSON.parse(foo) 
+    }
+
+    console.log(`=============== data4 Body transformToString: ${foo}`)
 
     const response:ResponseData = {
       success: true,
       message: `api/tests/s3 data!`,
-      data: { data1, data2, data3 },
+      data: { data1, data2, data3, data4: data4_ }
     }
     res.status(200).json(response)
   } catch (error) {
