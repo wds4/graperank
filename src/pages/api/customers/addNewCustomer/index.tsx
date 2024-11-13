532172fcfe98d97cb5b791674dcbf9780aafa575
David Strayhorn <david.strayhorn4@gmail.com>
2024-11-13 16:32:50 -0500

    edit insert command

--- src/pages/api/customers/addNewCustomer/index.tsx
+++ src/pages/api/customers/addNewCustomer/index.tsx
@@ -45,8 +45,8 @@ export default async function handler(
         });
 
         const command = ` INSERT INTO customers 
-('pubkey', 'whenSignedUp')
-VALUES ( '${pubkey1}', ${currentTimestamp} )
+(id, pubkey, whenSignedUp)
+VALUES ( 1, ${pubkey1}, ${currentTimestamp} )
 ;`
         const results = await connection.query(command);
         console.log(results);
