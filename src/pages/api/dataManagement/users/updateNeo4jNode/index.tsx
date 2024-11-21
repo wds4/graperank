5468ab6bca673da1f0b97c2c3f0b667c224c8c99
David Strayhorn <david.strayhorn4@gmail.com>
2024-11-20 20:43:25 -0500

    debugging

--- src/pages/api/dataManagement/users/updateNeo4jNode/index.tsx
+++ src/pages/api/dataManagement/users/updateNeo4jNode/index.tsx
@@ -56,7 +56,7 @@ export default async function handler(
       const pubkey_parent = oNextUser.pubkey
 
       // cypher1: add node pubkey_parent if not already exists
-      const cypher1 = await read(`MERGE (${pubkey_parent}:NostrUser {pubkey: '${pubkey_parent}'}) RETURN ${pubkey_parent}.pubkey AS pubkey `, {})
+      const cypher1 = await read(`MERGE (hodlhodlhodl:NostrUser {pubkey: '${pubkey_parent}'}) RETURN ${pubkey_parent}.pubkey AS pubkey `, {})
       console.log(cypher1)
       aCypherResults.push({cypher1})
 
