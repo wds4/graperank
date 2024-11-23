788bead94d230f2873ca36f30c655838a1be58e8
David Strayhorn <david.strayhorn4@gmail.com>
2024-11-23 11:52:27 -0500

    importCsv with TEST relationship

--- src/pages/api/tests/neo4j/importCsv/index.tsx
+++ src/pages/api/tests/neo4j/importCsv/index.tsx
@@ -1,3 +1,4 @@
+import { write } from '@/lib/neo4j'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 /*
@@ -8,12 +9,31 @@ https://graperank.tech/api/tests/neo4j/importCsv
 
 const cypher1 = `LOAD CSV FROM 'https://graperank.tech/api/tests/neo4j/generateCsv'
 AS row
-MERGE (n:NostrUser {pubkey: row[1]})-[:FOLLOWS]->(m:NostrUser {pubkey: row[2]})
+MERGE (n:NostrUser {pubkey: row[1]})-[:TEST]->(m:NostrUser {pubkey: row[2]})
 `
+
+type ResponseData = {
+  success: boolean,
+  message: string,
+  data?: object,
+}
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse
+  res: NextApiResponse<ResponseData>
 ) {
-  res.status(200).send(cypher1)
+  const cypher1_result = await write(cypher1, {})
+  console.log(`result: ${JSON.stringify(cypher1_result)}`)
+
+  const response:ResponseData = {
+    success: true,
+    message: 'api/tests/neo4j Hello from Next.js!',
+    data: {
+      cypher1,
+      cypher1_result,
+    }
+  }
+
+  res.status(200).json(response)
+
 }
