7607478db0828736f4088a0f5ff27d6c16c4196b
David Strayhorn <david.strayhorn4@gmail.com>
2024-11-23 12:42:26 -0500

    update cypher command in importCsv

--- src/pages/api/tests/neo4j/importCsv/index.tsx
+++ src/pages/api/tests/neo4j/importCsv/index.tsx
@@ -5,14 +5,40 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 to access:
 https://graperank.tech/api/tests/neo4j/importCsv
 
+LOAD CSV FROM 'https://graperank.tech/api/tests/neo4j/generateCsv'
+AS row
+MERGE (n:TestNostrUser {pubkey: row[1]})-[:TEST]->(m:TestNostrUser {pubkey: row[2]})
+
+LOAD CSV FROM 'https://graperank.tech/api/tests/neo4j/generateCsv'
+AS row
+MERGE (n:TestNostrUser {pubkey: row[1]})
+MERGE (m:TestNostrUser {pubkey: row[2]})
+MERGE (n)-[:TEST]->(m)
+
+LOAD CSV FROM 'https://graperank.tech/api/tests/neo4j/generateCsv'
+AS row
+MATCH (n:TestNostrUser {pubkey: row[1]}), (m:TestNostrUser {pubkey: row[2]})
+MERGE (n)-[:TEST]->(m)
+
+LOAD CSV FROM 'https://graperank.tech/api/tests/neo4j/generateCsv'
+AS row
+MERGE (n:TestNostrUser {pubkey: row[1]}), (m:TestNostrUser {pubkey: row[2]})
+MERGE (n)-[:TEST]->(m)
+
 MATCH (n)-[:TEST]->(m) RETURN n,m
 
 MATCH (n:TestNostrUser) RETURN n
+
+MATCH (n:TestNostrUser) DETACH DELETE n RETURN n
+
+CREATE CONSTRAINT testNostUserUniquePubkey FOR (n:TestNostrUser) REQUIRE n.pubkey IS UNIQUE
 */
 
 const cypher1 = `LOAD CSV FROM 'https://graperank.tech/api/tests/neo4j/generateCsv'
 AS row
-MERGE (n:TestNostrUser {pubkey: row[1]})-[:TEST]->(m:TestNostrUser {pubkey: row[2]})
+MERGE (n:TestNostrUser {pubkey: row[1]})
+MERGE (m:TestNostrUser {pubkey: row[2]})
+MERGE (n)-[:TEST]->(m)
 `
 
 type ResponseData = {
