db74dcd998158ab228eeeb844a2509e49bb2db52
David Strayhorn <david.strayhorn4@gmail.com>
2024-12-01 13:44:13 -0500

    expand fullWoT endpoint to 600 paths

--- src/pages/api/algos/dos/fullWoT/index.tsx
+++ src/pages/api/algos/dos/fullWoT/index.tsx
@@ -11,9 +11,6 @@ pubkey2: 5c624c471f52d737a1e9a74f598f681d41c43703741c260aa620fcbdb8995e31 // 5 h
 pubkey2: 1dda43d37807decafe62882615d82c22d674d5c8333a9eb314c73b6771b9224c // 9 hops away
 https://www.graperank.tech/api/algos/dos/fullWoT?pubkey1=e5272de914bd301755c439b88e6959a43c9d2664831f093c51e9c799a16a102f
 
-    MATCH p = shortestPath((:Person {name: "Alice"})-[:KNOWS*]->(:Person {name: "Bob"}))
-
-    RETURN length(p) - 1
 */
 
 type ResponseData = {
@@ -39,7 +36,7 @@ export default async function handler(
     if (typeof pubkey1 == 'string' && verifyPubkeyValidity(pubkey1)) {
       const cypher1 = `MATCH p = shortestPath((r:NostrUser {pubkey: '${pubkey1}'})-[:FOLLOWS*]->(n:NostrUser))
 WHERE r.pubkey <> n.pubkey 
-RETURN n, length(p) as numHops LIMIT 100`
+RETURN n, length(p) as numHops LIMIT 600`
       try {
         const result_cypher1 = await read(cypher1, {})
         console.log(result_cypher1)
@@ -47,6 +44,10 @@ RETURN n, length(p) as numHops LIMIT 100`
         const aResults = JSON.parse(JSON.stringify(result_cypher1))
 
         const aDoSWoT:string[][] = []
+        aDoSWoT[0] = []
+        aDoSWoT[1] = []
+        aDoSWoT[2] = []
+        aDoSWoT[3] = []
         for (let x=0; x < aResults.length; x++) {
           const numHops = aResults[x].numHops.low
           if (!aDoSWoT[numHops]) {
@@ -62,7 +63,16 @@ RETURN n, length(p) as numHops LIMIT 100`
           success: true,
           message: `api/algos/dos/fullWoT data:`,
           data: {
-            pubkey1, cypher: cypher1, aDoSWoT, cypherQueryResult: result_cypher1
+            pubkey1, 
+            cypher: cypher1,
+            counts: {
+              dos_0: aDoSWoT[0].length,
+              dos_1: aDoSWoT[1].length,
+              dos_2: aDoSWoT[2].length,
+              dos_3: aDoSWoT[3].length,
+            },
+            aDoSWoT, 
+            cypherQueryResult: result_cypher1
           }
         }
         res.status(200).json(response)
