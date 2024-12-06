44f59fc8d9aea6ddbc740a3cb9f752bab5c13931
David Strayhorn <david.strayhorn4@gmail.com>
2024-12-06 10:39:37 -0500

    bug fix

--- src/pages/api/outwardFacing/returnDataForSelfProfilePage/index.tsx
+++ src/pages/api/outwardFacing/returnDataForSelfProfilePage/index.tsx
@@ -65,7 +65,7 @@ export default async function handler(
         const aResults = JSON.parse(JSON.stringify(result_cypherDos))
         const dos = aResults[0].numHops.low
         let amIFollowing = false
-        if (aFollowPubkeys.contains(pk)) {
+        if (aFollowPubkeys.includes(pk)) {
           amIFollowing = true
         }
         const oFollowerData = [ pk, dos, amIFollowing ]
