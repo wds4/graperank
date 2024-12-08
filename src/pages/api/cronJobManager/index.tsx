8f2fba644d2774ad48ea899a77a86ccb47bde14a
David Strayhorn <david.strayhorn4@gmail.com>
2024-12-08 12:38:19 -0500

    modify cronJobManager

--- src/pages/api/cronJobManager/index.tsx
+++ src/pages/api/cronJobManager/index.tsx
@@ -116,10 +116,11 @@ export default async function handler(
     // if (aUsers5b.length > 0) { url = url5b } // 100
     if (aUsers4.length > 0) { url = url4 } // 1000
     if (aUsers3b.length > 0) { url = url3b } // 10
-    if (aEvents2.length > 0) { url = url2 } // 1000
-    if (aEvents2b.length > 0) { url = url2b } // 1000
-    if (numEvents1 > 0) { url = url1 } // 200
-    if (aUsers0.length > 0) { url = url0 } // 300
+    if (aUsers3.length > 0) { url = url3 } // 10
+    if (aEvents2.length > 1000) { url = url2 } // 1000
+    if (aEvents2b.length > 1000) { url = url2b } // 1000
+    if (numEvents1 > 200) { url = url1 } // 200
+    if (aUsers0.length > 300) { url = url0 } // 300
 
     console.log(`url: ${url}`)
     
