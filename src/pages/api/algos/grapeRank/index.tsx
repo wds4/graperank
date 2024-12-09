6f99292f359087b0ceae220bfc1c1a643250334d
David Strayhorn <david.strayhorn4@gmail.com>
2024-12-09 01:10:08 -0500

    increase size of reverseObserveeObject

--- src/pages/api/algos/grapeRank/index.tsx
+++ src/pages/api/algos/grapeRank/index.tsx
@@ -60,7 +60,7 @@ export default async function handler(
         });
 
         // STEP 1
-        const sql0 = `SELECT id, pubkey, reverseObserveeObject FROM users WHERE reverseObserveeObject IS NOT NULL LIMIT 20000; `
+        const sql0 = `SELECT id, reverseObserveeObject FROM users WHERE reverseObserveeObject IS NOT NULL LIMIT 50000; `
         const results_sql0 = await connection.query(sql0);
         const aUsers0 = JSON.parse(JSON.stringify(results_sql0[0]))
 
@@ -97,7 +97,6 @@ export default async function handler(
             referencePubkey: observer,
             numObserveeObjects: aUsers0.length,
             oRatingsReverseSizeInMB,
-            oRatingsReverse,
           }
         }
         res.status(200).json(response)
