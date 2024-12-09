a4d4c923458d8ddb5389734b7743b08ad04b15e4
David Strayhorn <david.strayhorn4@gmail.com>
2024-12-09 16:15:48 -0500

    one round of calculations at algos/grapeRank

--- src/pages/api/algos/grapeRank/index.tsx
+++ src/pages/api/algos/grapeRank/index.tsx
@@ -3,6 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import mysql from 'mysql2/promise'
 import { ResponseData } from '@/types'
 import { isValidStringifiedObject } from '@/helpers'
+import { convertInputToConfidence } from '@/helpers/grapevine'
 
 /*
 This endpoint is likely to be deprecated or reworked in favor of:
@@ -13,9 +14,13 @@ currently: 'f' or 'm'; MIGHT change to [1, 0.05] vs [0, 0.1]
 
 Rework this endpoint; start this endpoint at step 3:
 3. create reverseRatingsObject by aggregating each reverseObserveeObject on the fly; should take about 10 seconds
-4. use reverseRatingsObject as input into 
+4. initialize Scorecards
+5. use reverseRatingsObject as input into GrapeRank
 
-- STEP 1: sql1: SELECT id, pubkey, reverseObserveeObject FROM users WHERE reverseObserveeObject IS NULL NOT NULL 
+- STEP 1: 
+  sql0: SELECTE id FROM users WHERE pubkey='${observer}'
+  sql1: SELECT id, reverseObserveeObject FROM users WHERE reverseObserveeObject IS NULL NOT NULL 
+
 - STEP 2: combine results of sql1 into one large raw data object oRatingsReverse of format: [context][ratee][rater] = [score, confidence]
 
 
@@ -60,29 +65,76 @@ export default async function handler(
         });
 
         // STEP 1
-        const sql0 = `SELECT id, reverseObserveeObject FROM users WHERE reverseObserveeObject IS NOT NULL; `
+        const sql0 = `SELECT id FROM users WHERE pubkey='${observer}'; `
         const results_sql0 = await connection.query(sql0);
         const aUsers0 = JSON.parse(JSON.stringify(results_sql0[0]))
+        let observerId = -1
+        if (aUsers0[0]) {
+          observerId = aUsers0[0].id
+        }
+
+        const sql1 = `SELECT id, reverseObserveeObject FROM users WHERE reverseObserveeObject IS NOT NULL; `
+        const results_sql1 = await connection.query(sql1);
+        const aUsers1 = JSON.parse(JSON.stringify(results_sql0[1]))
 
         // STEP 2
         type RatingsReverse = {[key:string]:{[key:string]:string}}
         const oRatingsReverse:RatingsReverse = {}
+        // oScorecards: oScorecards[rateeId] = average, confidence; probably also average and input; keep all in array for convenience
+        // observer is logged in user; context is notSpam;
+        const oScorecards:{[key:string]:[number,number,number,number]} = {} // influence, confidence, average, input
         
-        // const aDataDepot = []
-        for (let x=0; x < aUsers0.length; x++) {
-          const oUserData = aUsers0[x]
+        // STEPs 3 and 4
+        const aDataDepot = []
+        for (let x=0; x < aUsers1.length; x++) {
+          const oUserData = aUsers1[x]
           const sReverseObserveeObject:string = oUserData.reverseObserveeObject
-          const raterId:number = oUserData.id
-          // aDataDepot.push({raterId, sReverseObserveeObject})
+          const observeeId:number = oUserData.id
+          aDataDepot.push({observeeId, sReverseObserveeObject})
           if (isValidStringifiedObject(sReverseObserveeObject)) {
-            oRatingsReverse[raterId] = JSON.parse(sReverseObserveeObject)
+            oRatingsReverse[observeeId] = JSON.parse(sReverseObserveeObject)
+            oScorecards[observeeId] = [0,0,0,0]
+          }
+        }
+        oScorecards[observerId] = [1,1,1,9999]
+
+        // STEP 5
+        // one round of GrapeRank
+        const attenuationFactor = 0.85
+        const rigor = 0.25
+        for (let g=0; g < aUsers1.length; g++) {
+          const observeeId = aUsers1[g]
+          const oRR = oRatingsReverse[observeeId]
+          const aRaters = Object.keys(oRR)
+          let weights = 0
+          let products = 0
+          for (let r=0; r < aRaters.length; r++) {
+            const raterId = aRaters[r]
+            const sRating = oRR[raterId]
+            let rating = 1
+            let ratingConfidence = 0.05
+            if (sRating == 'm') {
+              rating = 0
+              ratingConfidence = 0.1
+            }
+            const aRaterInfluence = oScorecards[raterId][0]
+            const weight = attenuationFactor * ratingConfidence * aRaterInfluence
+            const product = weight * rating
+            weights += weight 
+            products += product
+          }
+          let average = 0
+          if (weights) {
+            average = products / weights
+          }
+          const confidence = convertInputToConfidence(weights,rigor)
+          const influence = average * confidence
+          oScorecards[observeeId] = [influence, confidence, average, weights]
+          if (influence > 0) {
+            aDataDepot.push({g, observeeId, influence})
           }
         }
         
-
-
-
-
         const close_result = await connection.end()
         console.log(`closing connection: ${close_result}`)
 
@@ -95,8 +147,9 @@ export default async function handler(
           message: `api/algos/grapeRank data:`,
           data: {
             referencePubkey: observer,
-            numObserveeObjects: aUsers0.length,
+            numObserveeObjects: aUsers1.length,
             oRatingsReverseSizeInMB,
+            aDataDepot,
           }
         }
         res.status(200).json(response)
