a8b883c6ee30001276901b7ee1ee54196c94f8aa
David Strayhorn <david.strayhorn4@gmail.com>
2024-12-26 11:50:35 -0500

    add findDuplicates endpoint

---
+++ src/pages/api/tests/neo4j/findDuplicates/index.tsx
@@ -0,0 +1,59 @@
+import { write } from '@/lib/neo4j'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+/*
+to access:
+https://graperank.tech/api/tests/neo4j/findDuplicates
+
+
+// define graph
+MATCH (source:NostrUser)-[r:FOLLOWS]->(target:NostrUser)
+RETURN gds.graph.project(
+  'nostrPagerank',
+  source,
+  target
+)
+
+// estimate how much memory is required
+CALL gds.pageRank.write.estimate('nostrPagerank', {
+  writeProperty: 'pageRank',
+  maxIterations: 20,
+  dampingFactor: 0.85
+})
+YIELD nodeCount, relationshipCount, bytesMin, bytesMax, requiredMemory
+
+// stream output
+CALL gds.pageRank.stream('nostrPagerank')
+YIELD nodeId, score
+RETURN gds.util.asNode(nodeId).pubkey AS pubkey, score
+ORDER BY score DESC, pubkey ASC
+// took about 14 seconds
+*/
+
+const cypher1 = `MATCH (n:NostrUser), (m:NostrUser) WHERE lower(n.pubkey)=lower(m.pubkey) AND n.pubkey <> m.pubkey RETURN n,m`
+
+type ResponseData = {
+  success: boolean,
+  message: string,
+  data?: object,
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  const cypher1_result = await write(cypher1, {})
+  console.log(`result: ${JSON.stringify(cypher1_result)}`)
+
+  const response:ResponseData = {
+    success: true,
+    message: 'api/tests/neo4j/findDuplicates Hello from Next.js!',
+    data: {
+      cypher1,
+      cypher1_result,
+    }
+  }
+
+  res.status(200).json(response)
+
+}
