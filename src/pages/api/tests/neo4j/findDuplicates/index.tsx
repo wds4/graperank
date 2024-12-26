0f8c1e4525a2e2281dd90ae538151e620e0fb195
David Strayhorn <david.strayhorn4@gmail.com>
2024-12-26 11:58:17 -0500

    update findDuplicates

--- src/pages/api/tests/neo4j/findDuplicates/index.tsx
+++ src/pages/api/tests/neo4j/findDuplicates/index.tsx
@@ -4,30 +4,6 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 /*
 to access:
 https://graperank.tech/api/tests/neo4j/findDuplicates
-
-
-// define graph
-MATCH (source:NostrUser)-[r:FOLLOWS]->(target:NostrUser)
-RETURN gds.graph.project(
-  'nostrPagerank',
-  source,
-  target
-)
-
-// estimate how much memory is required
-CALL gds.pageRank.write.estimate('nostrPagerank', {
-  writeProperty: 'pageRank',
-  maxIterations: 20,
-  dampingFactor: 0.85
-})
-YIELD nodeCount, relationshipCount, bytesMin, bytesMax, requiredMemory
-
-// stream output
-CALL gds.pageRank.stream('nostrPagerank')
-YIELD nodeId, score
-RETURN gds.util.asNode(nodeId).pubkey AS pubkey, score
-ORDER BY score DESC, pubkey ASC
-// took about 14 seconds
 */
 
 const cypher1 = `MATCH (n:NostrUser), (m:NostrUser) WHERE lower(n.pubkey)=lower(m.pubkey) AND n.pubkey <> m.pubkey RETURN n,m`
@@ -44,12 +20,22 @@ export default async function handler(
 ) {
   const cypher1_result = await write(cypher1, {})
   console.log(`result: ${JSON.stringify(cypher1_result)}`)
+  const aResults = JSON.parse(JSON.stringify(cypher1_result))
+
+  const aPubkeyPairs = []
+  for (let x=0; x < aResults.length; x++) {
+    const oNextPair = aResults[x]
+    const pk1 = oNextPair.n.properties.pubkey
+    const pk2 = oNextPair.m.properties.pubkey
+    aPubkeyPairs.push({pk1,pk2})
+  }
 
   const response:ResponseData = {
     success: true,
     message: 'api/tests/neo4j/findDuplicates Hello from Next.js!',
     data: {
       cypher1,
+      aPubkeyPairs,
       cypher1_result,
     }
   }
