82690dc922c6f8265ccf8846a773fd8d89cc2441
David Strayhorn <david.strayhorn4@gmail.com>
2024-12-26 19:31:32 -0500

    adding nip51Events endpoing

--- src/pages/api/nostr/listeners/nip51Events/index.tsx
+++ src/pages/api/nostr/listeners/nip51Events/index.tsx
@@ -1,9 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import NDK, { NDKEvent, NDKFilter, NostrEvent } from '@nostr-dev-kit/ndk'
+import NDK, { NDKEvent, NDKFilter } from '@nostr-dev-kit/ndk' // NostrEvent
 import { validateEvent } from 'nostr-tools'
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
-import { makeEventSerializable } from '@/helpers'
+// import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
+// import { makeEventSerializable } from '@/helpers'
 
+/*
 const client = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
@@ -11,18 +12,43 @@ const client = new S3Client({
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
   },
 })
+*/
 
 /*
-This endpoint listens on nostr for kind 1984 events and:
+This endpoint listens on nostr for NIP-51 events and:
 - inserts events into S3
 
 A separate script is necessary to transfer event data into the relevant sql tables
 
+STANDARD LISTS
+10000 mute list
+10001 pinned notes
+10003 bookmarks
+10004 communities
+10005 public chats
+10006 blocked relays
+10007 search relays
+10009 simple groups
+10015 interests
+10030 emojis
+10050 DM relays
+10101 good wiki authors
+10102 good wiki relays
+
+SETS
+30000 follow sets
+30002 relay sets
+30003 bookmark sets
+30004 curation sets: articles
+30005 curation sets: videos
+30007 kind mute sets
+30015 interest sets
+30030 emoji sets
+30063 release artifact sets
+
 usage:
 
-http://localhost:3000/api/nostr/listeners/kind1984Events?t=3600
-
-https://graperank.tech/api/nostr/listeners/kind1984Events?t=3600
+https://graperank.tech/api/nostr/listeners/nip51Events?kind=10003&t=3600
 
 */
 
@@ -39,20 +65,26 @@ type ResponseData = {
   data?: object
 }
 
+/*
 const serializeEvent = async (event:NostrEvent) => {
   const oOutput = makeEventSerializable(event)
   const sOutput = JSON.stringify(oOutput)
   return sOutput
 }
+*/
  
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
   const searchParams = req.query
-  let numSeconds = 3600 // the default number of seconds; currently not implemented
+  let numSeconds = 3600 // the default number of seconds
+  let kinds = [10003]
   if (searchParams.t) {
     numSeconds = Number(searchParams.t)
+  }
+  if (searchParams.kind) {
+    kinds = [Number(searchParams.kind)]
   }
   const currentTimestamp = Math.floor(Date.now() / 1000)
   const sinceTimestamp = currentTimestamp - numSeconds
@@ -61,7 +93,7 @@ export default async function handler(
   console.log(startTimestamp)
   try {
     await ndk.connect()
-    const filter:NDKFilter = { kinds: [1984] }
+    const filter:NDKFilter = { kinds }
     const sub1 = ndk.subscribe(filter)
     const receivedEvents:string[] = []        
     sub1.on('event', async (event:NDKEvent) => {
@@ -69,6 +101,7 @@ export default async function handler(
         console.log(`received event.kind: ${event.kind}; event.id: ${event.id}`)
         receivedEvents.push(event.id)
         /* PutObjectCommand */
+        /*
         const params = {
           Bucket: 'grapevine-nostr-cache-bucket',
           Key: 'recentlyAddedEventsByEventId/' + event.id,
@@ -77,14 +110,17 @@ export default async function handler(
         const command_s3 = new PutObjectCommand(params);
         const data = await client.send(command_s3);
         console.log(`===== data: ${JSON.stringify(data)}`)
+        */
       }
     })
     sub1.on('eose', async () => {
       // await timeout(5000)
       const response = {
         success: true,
-        message: `api/tests/listeners/kind1984Events eose!`,
+        message: `api/tests/listeners/nip51Events eose!`,
         data: {
+          kinds,
+          numEvents: receivedEvents.length,
           receivedEvents,
         }
       }
@@ -93,7 +129,7 @@ export default async function handler(
   } catch (e) {
     const response = {
       success: false,
-      message: `api/tests/listeners/kind1984Events error: ${e}`
+      message: `api/tests/listeners/nip51Events error: ${e}`
     }
     res.status(500).json(response)
   }
