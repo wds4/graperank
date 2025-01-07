e7c69aecae7ce06c08e24c900dd26b9f02fb9992
David Strayhorn <david.strayhorn4@gmail.com>
2025-01-07 13:03:16 -0500

    bug fix

--- src/pages/api/tests/sql/index.tsx
+++ src/pages/api/tests/sql/index.tsx
@@ -40,7 +40,7 @@ export default async function handler(
           fields
         }
       }
-      res.status(500).json(response)
+      res.status(200).json(response)
     } catch (error) {
       console.log(`error: ${JSON.stringify(error)}`)
       const response:ResponseData = {
