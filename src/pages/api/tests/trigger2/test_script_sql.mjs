cbf54df2879bfac9f69e05160321bca076e4b60d
David Strayhorn <david.strayhorn4@gmail.com>
2025-01-09 18:57:55 -0500

    async secondSqlCommand in test_script_sql

--- src/pages/api/tests/trigger2/test_script_sql.mjs
+++ src/pages/api/tests/trigger2/test_script_sql.mjs
@@ -19,14 +19,13 @@ console.log(`command0 results: ${JSON.stringify(results, null, 4)}`);
 
 const currentTimestamp = Math.floor(Date.now() / 1000)
 
-const command1 = `UPDATE testTable SET whenupdated = ${currentTimestamp} WHERE name = 'fred'; `
-await connection.query(command1, (err, results) => {
-  if (err) {
-    console.error('Error executing command1 query:', err);
-    return;
-  }
+const secondSqlCommand = async () => {
+  const command1 = `UPDATE testTable SET whenupdated = ${currentTimestamp} WHERE name = 'fred'; `
+  const [results] = await connection.query(command1);
   console.log(`command1 results: ${JSON.stringify(results, null, 4)}`);
-});
+}
+
+await secondSqlCommand()
 
 /*
 connection.connect((err) => {
