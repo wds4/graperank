a4f5e3ba003790c90d83d3956e2ab2fdcbf0dbf1
David Strayhorn <david.strayhorn4@gmail.com>
2025-01-09 19:25:45 -0500

    update test_script_nostr

--- src/pages/api/tests/trigger2/test_script_nostr.mjs
+++ src/pages/api/tests/trigger2/test_script_nostr.mjs
@@ -1,8 +1,28 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 // const NDK = require('@nostr-dev-kit/ndk').NDK
 import NDK from '@nostr-dev-kit/ndk'
+import mysql from 'mysql2/promise'
 
 console.log('Node.js test_script_nostr.mjs executed!');
+
+const connection = await mysql.createConnection({
+  host: 'grapevine-nostr-cache-db.cp4a4040m8c9.us-east-1.rds.amazonaws.com',
+  port: 3306,
+  user: 'pgft',
+  password: 'vehZyt-cuspuj-8ruvma',
+  database: 'grapevineNostrCacheDb',
+});
+
+const runSqlCommand = async () => {
+  const command1 = `UPDATE testTable SET whenupdated = ${currentTimestamp} WHERE name = 'fred'; `
+  const [results] = await connection.query(command1);
+  console.log(`command1 results: ${JSON.stringify(results, null, 4)}`);
+}
+
+await runSqlCommand()
+
+const close_result = await connection.end()
+console.log(`closing connection: ${JSON.stringify(close_result)}`)
 
 const explicitRelayUrls = [
   'wss://purplepag.es',
@@ -14,7 +34,9 @@ const ndk = new NDK({explicitRelayUrls})
 const pubkey = 'e5272de914bd301755c439b88e6959a43c9d2664831f093c51e9c799a16a102f'
 
 try {
+    console.log('Node.js test_script_nostr.mjs A!');
     await ndk.connect()
+    console.log('Node.js test_script_nostr.mjs B!');
     const filter = { kinds: [3, 10000], authors: [pubkey], limit: 10 }
     const sub1 = ndk.subscribe(filter)
     const receivedEvents = []
