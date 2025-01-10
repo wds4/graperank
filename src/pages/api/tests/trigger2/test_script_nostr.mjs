2549d5180081469ac86eebbf4e71eb9c1554d457
David Strayhorn <david.strayhorn4@gmail.com>
2025-01-09 19:19:24 -0500

    edit test_script_nostr.mjs

--- src/pages/api/tests/trigger2/test_script_nostr.mjs
+++ src/pages/api/tests/trigger2/test_script_nostr.mjs
@@ -2,7 +2,7 @@
 // const NDK = require('@nostr-dev-kit/ndk').NDK
 import NDK from '@nostr-dev-kit/ndk'
 
-console.log('Node.js script executed!');
+console.log('Node.js test_script_nostr.mjs executed!');
 
 const explicitRelayUrls = [
   'wss://purplepag.es',
@@ -14,7 +14,7 @@ const ndk = new NDK({explicitRelayUrls})
 const pubkey = 'e5272de914bd301755c439b88e6959a43c9d2664831f093c51e9c799a16a102f'
 
 try {
-    ndk.connect()
+    await ndk.connect()
     const filter = { kinds: [3, 10000], authors: [pubkey], limit: 10 }
     const sub1 = ndk.subscribe(filter)
     const receivedEvents = []
@@ -25,7 +25,7 @@ try {
     sub1.on('eose', async () => {
       const response = {
         success: true,
-        message: `api/tests/nostr eose!`,
+        message: `test_script_nostr eose!`,
         data: receivedEvents
       }
       res.status(200).json(response)
@@ -33,7 +33,7 @@ try {
   } catch (e) {
     const response = {
       success: false,
-      message: `api/tests/nostr error: ${e}`
+      message: `test_script_nostr error: ${e}`
     }
     res.status(500).json(response)
   }
