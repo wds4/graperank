8644cd30fb810d4bf570b1b48fead2a3654a0fcb
David Strayhorn <david.strayhorn4@gmail.com>
2025-01-09 19:49:01 -0500

    add update script to test_script_nostr

--- src/pages/api/tests/trigger2/test_script_nostr.mjs
+++ src/pages/api/tests/trigger2/test_script_nostr.mjs
@@ -5,25 +5,26 @@ import mysql from 'mysql2/promise'
 
 console.log('Node.js test_script_nostr.mjs executed!');
 
-const connection = await mysql.createConnection({
-  host: 'grapevine-nostr-cache-db.cp4a4040m8c9.us-east-1.rds.amazonaws.com',
-  port: 3306,
-  user: 'pgft',
-  password: 'vehZyt-cuspuj-8ruvma',
-  database: 'grapevineNostrCacheDb',
-});
+const runSqlCommand = async (command) => {
+  const connection = await mysql.createConnection({
+    host: 'grapevine-nostr-cache-db.cp4a4040m8c9.us-east-1.rds.amazonaws.com',
+    port: 3306,
+    user: 'pgft',
+    password: 'vehZyt-cuspuj-8ruvma',
+    database: 'grapevineNostrCacheDb',
+  });
 
-const runSqlCommand = async () => {
-  const currentTimestamp = Math.floor(Date.now() / 1000)
-  const command1 = `UPDATE testTable SET whenupdated = ${currentTimestamp} WHERE name = 'fred'; `
-  const [results] = await connection.query(command1);
-  console.log(`command1 results: ${JSON.stringify(results, null, 4)}`);
+  const [results] = await connection.query(command);
+  console.log(`command results: ${JSON.stringify(results, null, 4)}`);
+
+  const close_result = await connection.end()
+
+  console.log(`closing connection: ${JSON.stringify(close_result)}`)
 }
 
-await runSqlCommand()
-
-const close_result = await connection.end()
-console.log(`closing connection: ${JSON.stringify(close_result)}`)
+const currentTimestamp = Math.floor(Date.now() / 1000)
+const command1 = `UPDATE testTable SET whenupdated = ${currentTimestamp} WHERE name = 'fred'; `
+await runSqlCommand(command1)
 
 const explicitRelayUrls = [
   'wss://purplepag.es',
@@ -50,6 +51,8 @@ const receivedEvents = []
 
 sub1.on('event', async (event) => {
   console.log(`event.id: ${event.id}`)
+  const command1 = `INSERT INTO testTable (name) VALUES ('jack'); `
+  await runSqlCommand(command1)
   receivedEvents.push(event.id)
 })
 
